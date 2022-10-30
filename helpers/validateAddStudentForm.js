85ca91706031ab0866741ebcf80fd5dfe8eeab84
Thomas White <thomas.white5@mohawkcollege.ca>
2022-10-30 15:06:04 -0400

    Added ability to add multiple grades for students - I KNOW, OUTSIDE BRANCH SCOPE! GAWD

--- helpers/validateAddStudentForm.js
+++ helpers/validateAddStudentForm.js
@@ -2,13 +2,13 @@ const validate = values => {
     const errors = {};
 
     if (!values.schoolName) {
-      errors.schoolName = 'Required';
+      errors.schoolName = 'This field is Required';
     } else if (values.schoolName.length > 100) {
         errors.schoolName = 'School name must be 100 characters or less';
     } 
   
     if (!values.studentName) {
-      errors.studentName = 'Required';
+      errors.studentName = 'This field is Required';
     } else if (values.studentName.length > 100) {
       errors.studentName = 'Student Name must be 100 characters or less';
     } 
@@ -17,9 +17,7 @@ const validate = values => {
         errors.newClass = 'Class must be 100 characters or less';
     } 
 
-    if (!/^[0-9]+$/i.test(values.newMark)) {
-        errors.newMark = 'Marks can only contain numbers';
-    } else if (values.newMark.length > 3) {
+    if (values.newMark.length > 3) {
         errors.newMark = 'Mark must be 3 characters or less';
     } 
   
